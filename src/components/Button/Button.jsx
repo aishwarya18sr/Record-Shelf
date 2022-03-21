594d365da8b9bcbbb844a97c62c3451f63a7db51
Aishwarya S R <Aishwarya_S_R@mckinsey.com>
2022-03-21 13:04:33 +0530

    feat: add Button component and improve home page

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Button.css';
+
+function Button() {
+  return (
+    <button className="buttonSync" type="submit">sync</button>
+  );
+}
+
+export default Button;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './HomePage.css';
 import Header from '../../components/Header/Header';
+import Button from '../../components/Button/Button';
 
 function HomePage() {
   return (
     <div>
       <Header />
+      <p className="homepageSimile">:((</p>
+      <p className="homepageText">seems a bit empty in here..</p>
+      <Button />
     </div>
   );
 }
