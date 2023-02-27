19fc6a31255880651e1eaf856ba17b193bd69f83
Aishwarya S R <Aishwarya.SR>
2023-02-27 22:50:56 +0530

    chore(record-shelf): add test cases for header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,7 +4,7 @@ import './Header.css';
 function Header() {
   return (
     <header className="recordsHeader">
-      <p className="recordsHeaderText">
+      <p className="recordsHeaderText" data-testid="records-header-text">
         My
         <span className="recordsHeaderSpan">Record</span>
         Shelf
